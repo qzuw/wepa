6b27ea9bb64bf274c327bb5b7b7b9ba4f9beee9f
Uotila Terho T <ttuotila@cc.helsinki.fi>
2016-11-24 15:34:57 +0200

    aloitus

---
+++ src/main/java/wepa/wepa/Wepa.java
@@ -0,0 +1,16 @@
+package wepa.wepa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Wepa {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(Wepa.class, args);
+    }
+
+}
