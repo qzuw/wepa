940bdd8509af89ec98330f75f2b5ee810034081c
Uotila Terho T <ttuotila@cc.helsinki.fi>
2016-11-25 17:37:38 +0200

    another go at Travis

--- src/main/java/wepa/wepa/controller/ExerciseController.java
+++ src/main/java/wepa/wepa/controller/ExerciseController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class ExerciseController {
+
     @RequestMapping("/ex")
     public String handleDefault() {
         return "fillExercises";
