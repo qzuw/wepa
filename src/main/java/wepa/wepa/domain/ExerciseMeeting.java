74d8ac622b9cc49f08499355b86769eacf8d9723
Palva M Henriikka <hpalva@da5-cs-bk107-30.helsinki.fi>
2016-11-25 13:13:56 +0200

    Course-, ExerciseMeeting-, WeeklyExercise-classes added

---
+++ src/main/java/wepa/wepa/domain/ExerciseMeeting.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package wepa.wepa.domain;
+
+public class ExerciseMeeting {
+    
+    private String id;
+    
+    public String getExerciseMeeting(){
+        return id;
+    }
+    
+    public void setExerciseMeeting(String id){
+        this.id = id;
+    }
+    
+}
---
+++ src/main/java/wepa/wepa/domain/WeeklyExercise.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package wepa.wepa.domain;
+
+public class WeeklyExercise {
+    
+    private String description;
+    
+    public String getWeeklyExercise(){
+        return description;
+    }
+    
+    public void setWeeklyExercise(String description){
+        this.description = description;
+    }
+}
