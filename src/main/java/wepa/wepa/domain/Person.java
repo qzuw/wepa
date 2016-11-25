9c6d9045bc45cb24c4578373ec57d15dfe9dfe5a
Palva M Henriikka <hpalva@da5-cs-bk107-30.helsinki.fi>
2016-11-25 12:59:28 +0200

    Person-class added

--- src/main/java/wepa/wepa/domain/Person.java
+++ src/main/java/wepa/wepa/domain/Person.java
@@ -6,4 +6,23 @@ import org.springframework.data.jpa.domain.AbstractPersistable;
 @Entity
 public class Person extends AbstractPersistable<Long>{
     
+    private int studentNumber;
+    private String name;
+    
+    public int getStudentNumber(){
+        return studentNumber;
+    }
+    
+    public void setStudentNumber(int number) {
+        this.studentNumber = number;
+    }
+    
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String nickname) {
+        this.name = nickname;
+    }
+    
 }
