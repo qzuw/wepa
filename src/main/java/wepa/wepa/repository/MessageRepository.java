b2004b1a322ceecb7ea25ede7075c3833dc090c5
Uotila Terho T <ttuotila@cc.helsinki.fi>
2016-11-28 15:09:36 +0200

    no messages into db

--- src/main/java/wepa/wepa/repository/MessageRepository.java
+++
@@ -1,8 +0,0 @@
-package wepa.wepa.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import wepa.wepa.domain.Message;
-
-public interface MessageRepository extends JpaRepository<Message, Long> {
-    
-}
