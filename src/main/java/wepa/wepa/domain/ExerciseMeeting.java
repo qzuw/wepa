1aabda1cec2d29e8d5713ef6700b5d473498617f
Uotila Terho T <ttuotila@cc.helsinki.fi>
2016-11-28 15:28:35 +0200

    cleanup + get&set

--- src/main/java/wepa/wepa/domain/ExerciseMeeting.java
+++ src/main/java/wepa/wepa/domain/ExerciseMeeting.java
@@ -1,24 +1,33 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package wepa.wepa.domain;
 
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import org.springframework.data.jpa.domain.AbstractPersistable;
 
 @Entity
 public class ExerciseMeeting extends AbstractPersistable<Long> {
     
     private String description;
+    @ManyToOne
+    @JoinColumn
+    private WeeklyExercise week;
     
-    public String getExerciseMeeting(){
+    public String getDescription() {
         return description;
     }
-    
-    public void setExerciseMeeting(String id){
-        this.description = id;
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public WeeklyExercise getWeek() {
+        return week;
+    }
+
+    public void setWeek(WeeklyExercise week) {
+        this.week = week;
     }
     
 }
--- src/main/java/wepa/wepa/domain/WeeklyExercise.java
+++ src/main/java/wepa/wepa/domain/WeeklyExercise.java
@@ -1,23 +1,34 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package wepa.wepa.domain;
 
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import org.springframework.data.jpa.domain.AbstractPersistable;
 
 @Entity
 public class WeeklyExercise extends AbstractPersistable<Long> {
 
     private String description;
+    @ManyToOne
+    @JoinColumn
+    private Course course;
 
-    public String getWeeklyExercise() {
+    public String getDescription() {
         return description;
     }
 
-    public void setWeeklyExercise(String description) {
+    public void setDescription(String description) {
         this.description = description;
     }
+
+    public Course getCourse() {
+        return course;
+    }
+
+    public void setCourse(Course course) {
+        this.course = course;
+    }
+    
+    
 }
