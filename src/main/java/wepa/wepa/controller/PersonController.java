3df0c81e49253ed15ae93dd90ad6cac0c8d59fed
Palva M Henriikka <henriikka.palva@hotmail.com>
2016-12-01 16:23:15 +0200

    Student can be added to the course

--- src/main/java/wepa/wepa/controller/PersonController.java
+++ src/main/java/wepa/wepa/controller/PersonController.java
@@ -7,13 +7,17 @@ package wepa.wepa.controller;
 
 import java.util.List;
 import javax.annotation.PostConstruct;
+import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import wepa.wepa.domain.Person;
+import wepa.wepa.domain.StudentExercise;
 import wepa.wepa.repository.PersonRepository;
 
 @Controller
@@ -47,10 +51,24 @@ public class PersonController {
         return "personList";
     }
 
-    @RequestMapping("/persons/{id}")
+    @RequestMapping(value = "/persons/{id}", method = RequestMethod.GET)
     public String getOnePerson(Model model, @PathVariable Long id) {
         model.addAttribute("person", personRepository.findOne(id));
         return "personInfo";
     }
-
+    
+    @RequestMapping(value = "/addperson", method = RequestMethod.GET)
+    public String addPersonForm() {
+        return "addperson";
+    }
+    
+    @RequestMapping(value = "/addperson", method = RequestMethod.POST)
+    public String postPerson(Model model, @RequestParam String studentNumber, @RequestParam String name) {
+        Person person = new Person();
+        person.setName(name);
+        person.setStudentNumber(studentNumber);
+        personRepository.save(person);
+        model.addAttribute("addedperson", person);
+        return "redirect:/addperson";
+    }
 }
