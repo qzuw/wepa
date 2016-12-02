963c92cc282eea4adae28735c73b6ffc0662e849
Palva M Henriikka <henriikka.palva@hotmail.com>
2016-12-02 15:55:29 +0200

    more PersonController tests

---
+++ src/test/java/wepa/wepa/controller/PersonControllerTest.java
@@ -0,0 +1,82 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package wepa.wepa.controller;
+
+import java.util.Arrays;
+import java.util.List;
+import static org.junit.Assert.assertTrue;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import wepa.wepa.domain.Person;
+import wepa.wepa.repository.PersonRepository;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PersonControllerTest {
+    
+    private MockMvc mock;
+    
+    @Autowired
+    private PersonRepository personRepository;
+    
+    @Autowired
+    private WebApplicationContext webAppContext;
+    
+    @Before
+    public void setUp(){
+        this.mock = MockMvcBuilders.webAppContextSetup(webAppContext).build();
+    }
+    
+    @Test
+    public void getOk() throws Exception {
+        mock.perform(get("/persons")).andExpect(status().is2xxSuccessful());
+    }
+    
+    @Test
+    public void getAddFormOk() throws Exception {
+        mock.perform(get("/addperson")).andExpect(status().is2xxSuccessful());
+    }
+    
+    @Test
+    public void getContainsPersons() throws Exception {
+        mock.perform(get("/persons")).andExpect(model().attributeExists("persons"));
+    }
+    
+    @Test
+    public void getFindsPerson() throws Exception {  
+        Person person = new Person();
+        person.setStudentNumber("000000000");
+        person.setName("Matti");
+        personRepository.save(person);
+        mock.perform(get("/persons/1")).andExpect(model().attributeExists("person"));
+    }
+    
+    @Test
+    public void postPerson() throws Exception {
+        mock.perform(post("/addperson").contentType(MediaType.APPLICATION_FORM_URLENCODED).param("studentNumber", "888888888").param("name", "Maija"));
+        
+        List<Person> persons = personRepository.findAll();
+        
+        boolean found = false;
+        
+        for(Person person : persons){
+            if(person.getStudentNumber().equals("888888888") && person.getName().equals("Maija")){
+                found = true;
+            }
+        }
+        assertTrue(found);
+    }
+}
