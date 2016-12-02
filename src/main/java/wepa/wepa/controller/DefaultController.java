7387ecf3796f5a25eb633491471a5993bf582ce7
Uotila Terho T <ttuotila@cc.helsinki.fi>
2016-12-02 18:04:31 +0200

    create default user if no users exist

--- src/main/java/wepa/wepa/controller/DefaultController.java
+++ src/main/java/wepa/wepa/controller/DefaultController.java
@@ -5,12 +5,37 @@
  */
 package wepa.wepa.controller;
 
-
+import java.util.Arrays;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import wepa.wepa.domain.Person;
+import wepa.wepa.repository.PersonRepository;
 
 @Controller
 public class DefaultController {
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+    @Autowired
+    private PersonRepository personRepository;
+
+    @PostConstruct
+    public void init() {
+
+        List<Person> users = personRepository.findAll();
+        if (users.isEmpty()) {
+            Person user = new Person();
+            user.setName("teach");
+            user.setStudentNumber("000000000");
+            user.setPassword(passwordEncoder.encode("teach"));
+            user.setAuthorities(Arrays.asList("TEACHER"));
+            personRepository.save(user);
+        }
+    }
 
     @RequestMapping("*")
     public String handleDefault() {
