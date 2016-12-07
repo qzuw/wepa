f18a9500d733d098d40b600e0a05f7531ab8c10f
Palva M Henriikka <henriikka.palva@hotmail.com>
2016-12-07 17:15:00 +0200

    exerciseForm changes

--- src/main/java/wepa/wepa/controller/ExerciseController.java
+++ src/main/java/wepa/wepa/controller/ExerciseController.java
@@ -2,9 +2,11 @@ package wepa.wepa.controller;
 
 import java.util.ArrayList;
 import java.util.List;
+import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -37,7 +39,7 @@ public class ExerciseController {
             person.setName(name);
             personRepository.save(person);
         }
-        
+
         StudentExercise studentExercise = new StudentExercise();
 
         studentExercise.setStudent(person);
