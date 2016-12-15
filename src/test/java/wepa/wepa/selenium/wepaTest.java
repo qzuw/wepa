ce3fd2591750c0c0b2324f80049faa83c8bcc989
Uotila Terho T <ttuotila@cc.helsinki.fi>
2016-12-15 19:22:05 +0200

    refactor test name

--- src/test/java/wepa/wepa/selenium/wepaTest.java
+++ src/test/java/wepa/wepa/selenium/wepaTest.java
@@ -18,7 +18,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-public class PersonTest extends FluentTest {
+public class wepaTest extends FluentTest {
  
     public WebDriver webDriver = new HtmlUnitDriver();
     
