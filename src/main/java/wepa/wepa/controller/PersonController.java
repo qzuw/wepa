beb75d0f27fc4d9637e5253d398a771138597131
Palva M Henriikka <henriikka.palva@hotmail.com>
2016-12-22 17:05:53 +0200

    Pagination to person list

--- src/main/java/wepa/wepa/controller/PersonController.java
+++ src/main/java/wepa/wepa/controller/PersonController.java
@@ -11,6 +11,7 @@ import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -26,6 +27,7 @@ import wepa.wepa.domain.Log;
 import wepa.wepa.domain.Person;
 import wepa.wepa.repository.LogRepository;
 import wepa.wepa.repository.PersonRepository;
+import wepa.wepa.service.PersonService;
 
 @Controller
 @RequestMapping("/persons")
@@ -40,18 +42,44 @@ public class PersonController {
     @Autowired
     private LogRepository logRepository;
 
+    @Autowired
+    private PersonService personService;
+
     @ModelAttribute
     private Person getPerson() {
         return new Person();
     }
-    
-    @RequestMapping(method = RequestMethod.GET)
-    public String getPersons(Model model) {
 
-        List<Person> persons = personRepository.findAll();
+    @RequestMapping(value = "/page/{pageNumber}", method = RequestMethod.GET)
+    public String getPersons(@PathVariable Integer pageNumber, Model model) {
+        if (pageNumber < 1) {
+            return "redirect:/persons/page/1";
+        }
+        Page<Person> page = personService.getPersonLog(pageNumber - 1);
+        int current = page.getNumber() + 1;
+        int previous = current - 1;
+        int next = current + 1;
+        if (previous >= 1) {
+            model.addAttribute("previous", previous);
+        }
+
+        int begin = Math.max(1, current - 5);
+        int end = Math.min(begin + 10, page.getTotalPages());
+        
+        if (next <= end) {
+            model.addAttribute("next", next);
+        }
+
+        model.addAttribute("personLog", page);
+        model.addAttribute("beginIndex", begin);
+        model.addAttribute("endIndex", end);
+        model.addAttribute("currentIndex", current);
+
+        List<Person> persons = page.getContent();
 
         model.addAttribute("persons", persons);
         return "person/personList";
+
     }
 
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
@@ -66,16 +94,16 @@ public class PersonController {
     }
 
     @RequestMapping(value = "/add", method = RequestMethod.POST)
-    public String postPerson(Model model, 
-            @RequestParam String studentNumber, 
-            @RequestParam String name, 
-            @Valid @ModelAttribute Person addedPerson, 
+    public String postPerson(Model model,
+            @RequestParam String studentNumber,
+            @RequestParam String name,
+            @Valid @ModelAttribute Person addedPerson,
             BindingResult bindingResult) {
-        
+
         if (bindingResult.hasErrors()) {
             return "person/addperson";
         }
-        
+
         Person person = addedPerson;
         person.setAuthorities(Arrays.asList("STUDENT"));
         personRepository.save(person);
--- src/main/java/wepa/wepa/repository/PersonRepository.java
+++ src/main/java/wepa/wepa/repository/PersonRepository.java
@@ -1,5 +1,7 @@
 package wepa.wepa.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import wepa.wepa.domain.Person;
 
@@ -8,4 +10,5 @@ public interface PersonRepository extends JpaRepository<Person, Long> {
     Person findByName(String name);
     
     Person findByStudentNumber(String studentNumber);
+    
 }
