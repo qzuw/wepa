4b6562c9abfe0d6ac7b8e38a89dc8c7d535d6a33
Uotila Terho T <ttuotila@cc.helsinki.fi>
2016-12-27 18:42:06 +0200

    fix test data

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -26,9 +26,9 @@ INSERT INTO Person (name, student_number) VALUES ('Student R', '222222239');
 INSERT INTO Course (name, course_start, course_end) VALUES ('Test course A', '2016-12-9', '2017-1-19');
 INSERT INTO Course (name, course_start, course_end) VALUES ('Test course B', '1998-9-1', '2020-5-19');
 
-INSERT INTO Course_assistants (courses_assisted_id, assistants_id) VALUES (1, 1);
-INSERT INTO Course_assistants (courses_assisted_id, assistants_id) VALUES (2, 1);
-INSERT INTO Course_assistants (courses_assisted_id, assistants_id) VALUES (2, 2);
+INSERT INTO Course_assistants (courses_assisted_id, assistants_id) VALUES (1, 3);
+INSERT INTO Course_assistants (courses_assisted_id, assistants_id) VALUES (2, 3);
+INSERT INTO Course_assistants (courses_assisted_id, assistants_id) VALUES (2, 4);
 
 INSERT INTO Course_students (courses_attended_id, students_id) VALUES (1, 5);
 INSERT INTO Course_students (courses_attended_id, students_id) VALUES (2, 5);
