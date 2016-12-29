234e170c9820a1fffc0a37ed4f3c0d27a04e3908
Uotila Terho T <ttuotila@cc.helsinki.fi>
2016-12-29 15:16:46 +0200

    1st fluentlentium test

--- src/test/java/wepa/wepa/selenium/wepaTest.java
+++ src/test/java/wepa/wepa/selenium/wepaTest.java
@@ -1,6 +1,7 @@
-
 package wepa.wepa.selenium;
 
+import java.util.Arrays;
+import java.util.UUID;
 import org.fluentlenium.adapter.FluentTest;
 import org.junit.After;
 import org.junit.AfterClass;
@@ -11,39 +12,68 @@ import static org.junit.Assert.*;
 import org.junit.runner.RunWith;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.embedded.LocalServerPort;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.context.junit4.SpringRunner;
-
+import wepa.wepa.domain.Person;
+import wepa.wepa.repository.PersonRepository;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class wepaTest extends FluentTest {
- 
+
     public WebDriver webDriver = new HtmlUnitDriver();
-    
+
     public WebDriver getDefaultDriver() {
         return webDriver;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
     @LocalServerPort
     private Integer port;
 
+    @Autowired
+    private PersonRepository personRepository;
+
+    private String personname;
+
+    @Before
+    public void setUp() {
+        Person testperson = new Person();
+        personname = UUID.randomUUID().toString();
+        testperson.setName(personname);
+        testperson.setPassword(passwordEncoder().encode(personname));
+        testperson.setStudentNumber("987654321");
+        testperson.setAuthorities(Arrays.asList("TEACHER"));
+        personRepository.save(testperson);
+
+    }
+
     @Test
-    public void addingExercisesNewStudent() {
-//        goTo("http://localhost:" + port + "/login");
-//        
-//        fill(find("#username")).with("nakki");
-//        fill(find("#password")).with("nakki");
-//        submit(find("form").first());
-//        
-//        
-//        goTo("http://localhost:" + port + "/persons");
-//
-//
-//        assertFalse(pageSource().contains("555555555"));
-//        assertFalse(pageSource().contains("Maija"));
-//        
+    public void loginTest() {
+        goTo("http://localhost:" + port + "/login");
+
+        fill(find("input")).with(personname);
+        fill(find("input")).with(personname);
+        submit(find("form").first());
+
+        goTo("http://localhost:" + port + "/persons/" + personRepository.findByName(personname).getId());
+
+        assertFalse(pageSource().contains("555555555"));
+        assertFalse(pageSource().contains("Maija"));
+        assertTrue(pageSource().contains(personname));
+
+    }
+
+    //        
 //        goTo("http://localhost:" + port + "/exercises");
 //
 //        fill(find("#studentNumber")).with("555555555");
@@ -56,7 +86,4 @@ public class wepaTest extends FluentTest {
 //        assertTrue(pageSource().contains("555555555"));
 //        assertTrue(pageSource().contains("Maija"));
 
-    }
-
-    
 }
