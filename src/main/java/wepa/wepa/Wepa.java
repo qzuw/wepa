8363d55a1139a15181743b958b8e108df8e47727
Uotila Terho T <ttuotila@cc.helsinki.fi>
2016-12-30 13:59:48 +0200

    lang?

--- src/main/java/wepa/wepa/Wepa.java
+++ src/main/java/wepa/wepa/Wepa.java
@@ -1,7 +1,13 @@
 package wepa.wepa;
 
+import java.util.Locale;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.WebMvcProperties.LocaleResolver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @SpringBootApplication
 public class Wepa {
@@ -11,6 +17,24 @@ public class Wepa {
      */
     public static void main(String[] args) {
         SpringApplication.run(Wepa.class, args);
+    }
+
+    @Bean
+    public SessionLocaleResolver localeResolver() {
+        SessionLocaleResolver slr = new SessionLocaleResolver();
+        slr.setDefaultLocale(Locale.US);
+        return slr;
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
+        lci.setParamName("lang");
+        return lci;
+    }
+
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
     }
 
 }
